2fd3bccb5af29294c3e89c0b4e9b0c3968d8deba
Nopsled <78918436+Nopsled@users.noreply.github.com>
2023-03-18 13:21:12 +0100

    Update Proxy.js

--- TamperMonkey (client)/Proxy.js
+++ TamperMonkey (client)/Proxy.js
@@ -4,12 +4,18 @@
 // @grant        none
 // ==/UserScript==
 
-(function (){
-    let ws = window.WebSocket;
-    class OasisSocket extends ws {
-        constructor(){
+(function () {
+    // Save the original WebSocket in a variable
+    const OriginalWebSocket = window.WebSocket;
+
+    // Create a custom WebSocket class that extends the original WebSocket
+    class OasisSocket extends OriginalWebSocket {
+        constructor() {
+            // Connect to the local proxy server
             super("ws://127.0.0.1:3000");
         }
     }
+
+    // Replace the global WebSocket with the custom OasisSocket
     window.WebSocket = OasisSocket;
 })();
